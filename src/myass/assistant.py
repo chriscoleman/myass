67756d9ae4bf63231adad6f5adea617eb2ac8f88
Chris Coleman <134727805+chriscoleman@users.noreply.github.com>
2023-09-04 19:17:48 +0100

    function calling

--- src/myass/assistant.py
+++ src/myass/assistant.py
@@ -1,4 +1,5 @@
 import copy
+import json
 
 import oaix
 
@@ -11,13 +12,24 @@ class Assistant:
         self.api = oaix.Api()
         self.messages = []
 
-    def __call__(self, content=None, messages=None):
+    def __call__(self, content):
         params = copy.deepcopy(self.config.flatten())
-        if messages is not None:
-            self.messages.extend(messages)
-        if content is not None:
-            self.messages.append({'role': 'user', 'content': content})
+        self.messages.append({'role': 'user', 'content': content})
         params['messages'].extend(self.messages)
         r = self.api.post('chat/completions', json=params)
-        self.messages.append(r['choices'][0]['message'])
-        return r['choices'][0]['message']['content']
+        m = r['choices'][0]['message']
+        while m.get('function_call') is not None:
+            fn = m['function_call']['name']
+            try:
+                f = getattr(self, fn)
+            except AttributeError:
+                r = None
+            else:
+                r = f(**json.loads(m['function_call']['arguments']))
+            params['messages'].append({'role': 'function',
+                                       'name': fn, 'content': r})
+            r = self.api.post('chat/completions', json=params)
+            params['messages'].pop()
+            m = r['choices'][0]['message']
+        self.messages.append(m)
+        return m['content']
